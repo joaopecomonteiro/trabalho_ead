47af24fd2d541636ad2be81c9907dd6745224cd5
DiogoT21 <diogoteixeira1999@gmail.com>
2025-05-22 15:29:18 +0100

    Update Trabalho2.R

--- Trabalho2.R
+++ Trabalho2.R
@@ -11,6 +11,7 @@ library(fpc)
 df <- read.csv("water_pollution_disease.csv")
 
 # Pre-Processing
+# Target Creation
 
 df$disease_burden <- df$Diarrheal.Cases.per.100.000.people +
   df$Cholera.Cases.per.100.000.people +
@@ -47,11 +48,8 @@ table(df_clean$disease_burden_class)
 
 # KMeans
 
-
-# Assuming df_clean is already prepared
 numeric_features <- df_clean[, sapply(df_clean, is.numeric)]
-
-# Remove the target (if still present)
+# Remove the target 
 numeric_features <- numeric_features[, !names(numeric_features) %in% "disease_burden_class"]
 
 # Scale the features
@@ -70,8 +68,6 @@ plot(1:20, wss, type = "b", pch = 19, frame = FALSE,
      ylab = "Total Within-Cluster Sum of Squares (WSS)",
      main = "Elbow Method for Optimal K")
 
-
-library(cluster)
 
 avg_sil <- vector()
 for (k in 2:10) {
@@ -107,6 +103,11 @@ pca <- prcomp(scaled_data, scale. = TRUE)
 pca_data <- as.data.frame(pca$x[, 1:3])
 pca_data$cluster <- as.factor(kmeans_result$cluster)
 
+summary(pca)
+# cada pca explica apenas 5%
+cor(scaled_data)
+# vars não correlacionadas
+
 fviz_cluster(kmeans_result, data = scaled_data, geom = "point", ellipse.type = "convex") +
   theme_minimal() +
   ggtitle(paste("K-means Clustering com K =", best_k))
@@ -117,9 +118,7 @@ res <- cluster.stats(dist(scaled_data), kmeans_result$cluster)
 
 cat("Calinski-Harabasz Index:", res$ch, "\n")
 cat("Silhouette médio:", res$avg.silwidth, "\n")
-# Um valor de 0.0385 é muito baixo, sugerindo que os clusters estão mal definidos, com muita sobreposição. Este é um sinal claro de que o agrupamento pode não estar a capturar estrutura real nos dados.
 cat("Índice de Dunn:", res$dunn, "\n")
-# Um valor abaixo de 1 (como 0.248) é baixo, indicando que os clusters estão muito próximos uns dos outros
 cat("Within-cluster sum of squares:", res$within.cluster.ss, "\n")
 
 
@@ -129,7 +128,7 @@ gap_stat <- clusGap(scaled_data,
                     FUN = kmeans,
                     nstart = 25,
                     K.max = 10,
-                    B = 5)  # número de amostragens bootstrap
+                    B = 5) 
 
 # Ver os resultados
 print(gap_stat, method = "firstmax")
@@ -137,6 +136,7 @@ print(gap_stat, method = "firstmax")
 # Plot do Gap Statistic
 fviz_gap_stat(gap_stat) +
   ggtitle("Gap Statistic para Determinar o Número Ótimo de Clusters")
+# O melhor é só 1, 2 seria o seguinte, ainda assim, tendo em conta o intervalo de confiança, há sobreposição
 
 
 # Hierarchical
@@ -164,7 +164,7 @@ hc2 <- hclust(dist_matrix, method = "complete")
 
 plot(hc2, labels = FALSE, hang = -1, main = "Hierarchical Clustering Dendrogram")
 
-data$hclust_cluster <- cutree(hc2, k = 10)
+df_clean$hclust_cluster <- cutree(hc2, k = 10)
 
 rect.hclust(hc2, k = 10, border = 2:11) 
 
@@ -182,11 +182,11 @@ plot(2:10, sil_widths, type = "b", pch = 19,
      ylab = "Silhueta média",
      main = "Silhueta para escolher K")
 
-# k=7
-abline(v = 7, col = "red", lty = 2)
+# k=2
+abline(v = 2, col = "red", lty = 2)
 
-# Cortar a árvore em 5 clusters
-clusters_hc <- cutree(hc, k = 7)
+# Cortar a árvore em 2 clusters
+clusters_hc <- cutree(hc, k = 2)
 
 # Adicionar clusters ao dataset
 df_clean$HC_Cluster <- as.factor(clusters_hc)
@@ -195,13 +195,20 @@ df_clean$HC_Cluster <- as.factor(clusters_hc)
 res <- cluster.stats(dist_matrix, clusters_hc)
 
 cat("Calinski-Harabasz Index:", res$ch, "\n")
-#  Um valor de 18.8 é baixo, sugerindo baixa separação entre grupos neste modelo hierárquico. No KMeans tinhas um valor 121.76, bem mais alto.
 cat("Silhouette médio:", res$avg.silwidth, "\n")
-# Um valor negativo indica que muitos pontos estão mais próximos de clusters vizinhos do que do seu próprio → estrutura de cluster fraca ou inexistente
 cat("Índice de Dunn:", res$dunn, "\n")
-# Este valor, embora ligeiramente melhor que no KMeans (0.248), continua fraco.
 cat("Within-cluster sum of squares:", res$within.cluster.ss, "\n")
-# Este valor é ligeiramente pior que o do KMeans (57,639.07), sugerindo menos coesão interna.
+
+
+# Results
+
+# C-H: O índice é muito maior em K-Means, o que indica que este método obteve melhor separação entre clusters e maior coesão interna.
+# Silhouette: O valor é extremamente baixo em ambos (< 0.1), o que significa que os clusters não estão bem definidos.
+# Dunn: Ambos os valores são muito baixos (idealmente > 1), indicando clusters próximos uns dos outros e fraca separação. O método hierárquico tem um valor ligeiramente melhor, mas ainda insatisfatório.
+# W-C: K-Means apresenta menor variabilidade dentro dos clusters, o que significa maior coesão interna.
+# CONCLUSION: Nenhum método mostra uma estrutura de clustering clara nos dados, mas K-Means destaca-se em 2 dos 4 indicadores (e com mais diferença), sendo uma escolha ligeiramente melhor se tiveres de optar por um método de agrupamento entre os dois. Ainda assim, os dados podem não ter clusters naturais fortes, e considerar outros métodos ou transformações pode ser o passo seguinte.
+
+
 
 # Estabilidade dos clusters (Jaccard Index)
 set.seed(123)
@@ -209,22 +216,21 @@ clusterboot_result <- clusterboot(
   dist_matrix, 
   clustermethod = disthclustCBI,
   method = "ward.D2", 
-  k = 5,
+  k = 2,
   B = 100
 )
 
 cat("Estabilidade (Jaccard médio por cluster):\n")
 print(clusterboot_result$bootmean)
-# Estes valores (~0.15–0.20) são muito baixos, o que significa que:
-#Os clusters não são consistentes entre diferentes amostras (bootstraps).
-#A estrutura de agrupamento é provavelmente espúria ou sensível a ruído.
+# A estrutura dos clusters produzida pelo clustering hierárquico com k = 2 não é estável, especialmente o primeiro cluster. Isto reforça os outros indicadores que também apontavam fraca separação entre grupos. Talvez os dados não tenham uma estrutura natural de agrupamento clara
 
 
-## Gaussian Mixture Models (EM algorithm) 
 
-# G = 7 clusters (escolhido previamente)
-mod7 <- Mclust(scaled_data, G = 7)
+# Gaussian Mixture Models (EM algorithm) 
+
+mod7 <- Mclust(scaled_data)
 summary(mod7)
+plot(mod7)
 
 # Visualização dos clusters em PC1 vs PC2
 df7 <- as.data.frame(scaled_data)
@@ -237,24 +243,56 @@ scores$em_cluster <- df7$em_cluster
 ggplot(scores, aes(x = Comp.1, y = Comp.2, color = em_cluster)) +
   geom_point() +
   theme_minimal() +
-  labs(title = "EM Clustering com 7 Clusters", x = "PC1", y = "PC2", color = "Cluster")
+  labs(title = "EM Clustering com 2 Clusters", x = "PC1", y = "PC2", color = "Cluster")
 
-#Avaliação (para G = 7)
+# Avaliação com métricas de cluster (para G = 2)
 
 cl7 <- mod7$classification
 dist_mat <- dist(scaled_data)
 stats7 <- cluster.stats(dist_mat, cl7)
 sil7 <- mean(silhouette(cl7, dist_mat)[, 3])
 
-cat("Métricas para G = 7:\n")
+cat("Métricas para G = 2:\n")
 cat("BIC:", mod7$bic, "\n")
 cat("Calinski-Harabasz Index:", stats7$ch, "\n")
+# GMM tem melhor separação que o modelo hierárquico, mas pior que K-Means.
 cat("Silhouette médio:", sil7, "\n")
+# Ainda é um valor muito baixo, indicando fraca estrutura de clusters, mas é ligeiramente melhor no GMM.
 cat("Dunn Index:", stats7$dunn, "\n")
+# GMM tem o pior índice de Dunn dos três — ou seja, os clusters estão ainda menos separados do que nos outros métodos.
 
 
-#-------------------------
+
+# LDA and QDA
+
+
+
 df <- read.csv("water_pollution_disease.csv")
+
+# Pre-Processing
+# Target Creation
+
+df$disease_burden <- df$Diarrheal.Cases.per.100.000.people +
+  df$Cholera.Cases.per.100.000.people +
+  df$Typhoid.Cases.per.100.000.people
+
+df$disease_burden_class <- cut(df$disease_burden,
+                               breaks = c(0, 150, 300, 450, Inf),
+                               labels = c("Very Low", "Low", "High", "Very High"),
+                               include.lowest = TRUE)
+
+df$disease_burden_class <- as.factor(df$disease_burden_class)
+
+
+df_clean <- df[, !(names(df) %in% "disease_burden")]
+
+df_clean <- na.omit(df_clean)
+
+df_clean$Country <- as.factor(df_clean$Country)
+df_clean$Region <- as.factor(df_clean$Region)
+df_clean$Water.Source.Type <- as.factor(df_clean$Water.Source.Type)
+df_clean$Water.Treatment.Method <- as.factor(df_clean$Water.Treatment.Method)
+
 
 set.seed(123)
 trainIndex <- createDataPartition(df_clean$disease_burden_class, p = 0.8, list = FALSE)
@@ -272,7 +310,7 @@ lda_pred <- predict(lda_model, newdata = testData)
 # Step 10: Evaluate performance
 confusionMatrix(lda_pred$class, testData$disease_burden_class)
 
-# Optional: Visualize the result
+# Visualize the result
 lda_df <- data.frame(lda_pred$x, Class = testData$disease_burden_class)
 ggplot(lda_df, aes(x = LD1, y = LD2, color = Class)) +
   geom_point(size = 3, alpha = 0.7) +
@@ -290,7 +328,6 @@ qda_pred <- predict(qda_model, newdata = testData)
 
 # Evaluate performance
 confusionMatrix(qda_pred$class, testData$disease_burden_class)
-
 
 
 
